8a5825004889a6ba34c21e2aae30eb95c4a9ccf0
annelyah <119406815+annelyah23@users.noreply.github.com>
2023-02-13 10:21:59 +0800

    Update update.sh

--- menu/update.sh
+++ menu/update.sh
@@ -4,29 +4,29 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 ###########- COLOR CODE -##############
 echo -e " [INFO] Downloading Update File"
 sleep 2
-wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/arismaramar/multi/main/menu/menu.sh" && chmod +x /usr/bin/menu
-wget -q -O /usr/bin/menu-ss "https://raw.githubusercontent.com/arismaramar/multi/main/menu/menu-ss.sh" && chmod +x /usr/bin/menu-ss
-wget -q -O /usr/bin/menu-vmess "https://raw.githubusercontent.com/arismaramar/multi/main/menu/menu-vmess.sh" && chmod +x /usr/bin/menu-vmess
-wget -q -O /usr/bin/menu-vless "https://raw.githubusercontent.com/arismaramar/multi/main/menu/menu-vless.sh" && chmod +x /usr/bin/menu-vless
-wget -q -O /usr/bin/menu-trojan "https://raw.githubusercontent.com/arismaramar/multi/main/menu/menu-trojan.sh" && chmod +x /usr/bin/menu-trojan
-wget -q -O /usr/bin/menu-xtls "https://raw.githubusercontent.com/arismaramar/multi/main/menu/menu-xtls.sh" && chmod +x /usr/bin/menu-xtls
-wget -q -O /usr/bin/menu-bot "https://raw.githubusercontent.com/arismaramar/multi/main/menu/menu-bot.sh" && chmod +x /usr/bin/menu-bot
-wget -q -O /usr/bin/menu-ssh "https://raw.githubusercontent.com/arismaramar/multi/main/menu/menu-ssh.sh" && chmod +x /usr/bin/menu-ssh
-wget -q -O /usr/bin/menu-set "https://raw.githubusercontent.com/arismaramar/multi/main/menu/menu-set.sh" && chmod +x /usr/bin/menu-set
-wget -q -O /usr/bin/menu-theme "https://raw.githubusercontent.com/arismaramar/multi/main/menu/menu-theme.sh" && chmod +x /usr/bin/menu-theme
-wget -q -O /usr/bin/menu-backup "https://raw.githubusercontent.com/arismaramar/multi/main/menu/menu-backup.sh" && chmod +x /usr/bin/menu-backup
-wget -q -O /usr/bin/menu-ip "https://raw.githubusercontent.com/arismaramar/multi/main/menu/menu-ip.sh" && chmod +x /usr/bin/menu-ip
-wget -q -O /usr/bin/menu-tor "https://raw.githubusercontent.com/arismaramar/multi/main/menu/menu-tor.sh" && chmod +x /usr/bin/menu-tor
-wget -q -O /usr/bin/autoboot "https://raw.githubusercontent.com/arismaramar/multi/main/menu/autoboot.sh" && chmod +x /usr/bin/autoboot
-wget -q -O /usr/bin/menu-tcp "https://raw.githubusercontent.com/arismaramar/multi/main/menu/menu-tcp.sh" && chmod +x /usr/bin/menu-tcp
-wget -q -O /usr/bin/rebootvps "https://raw.githubusercontent.com/arismaramar/multi/main/menu/rebootvps.sh" && chmod +x /usr/bin/rebootvps
-wget -q -O /usr/bin/menu-dns "https://raw.githubusercontent.com/arismaramar/multi/main/menu/menu-dns.sh" && chmod +x /usr/bin/menu-dns
+wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/annelyah23/snip/main/menu/menu.sh" && chmod +x /usr/bin/menu
+wget -q -O /usr/bin/menu-ss "https://raw.githubusercontent.com/annelyah23/snip/main/menu/menu-ss.sh" && chmod +x /usr/bin/menu-ss
+wget -q -O /usr/bin/menu-vmess "https://raw.githubusercontent.com/annelyah23/snip/main/menu/menu-vmess.sh" && chmod +x /usr/bin/menu-vmess
+wget -q -O /usr/bin/menu-vless "https://raw.githubusercontent.com/annelyah23/snip/main/menu/menu-vless.sh" && chmod +x /usr/bin/menu-vless
+wget -q -O /usr/bin/menu-trojan "https://raw.githubusercontent.com/annelyah23/snip/main/menu/menu-trojan.sh" && chmod +x /usr/bin/menu-trojan
+wget -q -O /usr/bin/menu-xtls "https://raw.githubusercontent.com/annelyah23/snip/main/menu/menu-xtls.sh" && chmod +x /usr/bin/menu-xtls
+wget -q -O /usr/bin/menu-bot "https://raw.githubusercontent.com/annelyah23/snip/main/menu/menu-bot.sh" && chmod +x /usr/bin/menu-bot
+wget -q -O /usr/bin/menu-ssh "https://raw.githubusercontent.com/annelyah23/snip/main/menu/menu-ssh.sh" && chmod +x /usr/bin/menu-ssh
+wget -q -O /usr/bin/menu-set "https://raw.githubusercontent.com/annelyah23/snip/main/menu/menu-set.sh" && chmod +x /usr/bin/menu-set
+wget -q -O /usr/bin/menu-theme "https://raw.githubusercontent.com/annelyah23/snip/main/menu/menu-theme.sh" && chmod +x /usr/bin/menu-theme
+wget -q -O /usr/bin/menu-backup "https://raw.githubusercontent.com/annelyah23/snip/main/menu/menu-backup.sh" && chmod +x /usr/bin/menu-backup
+wget -q -O /usr/bin/menu-ip "https://raw.githubusercontent.com/annelyah23/snip/main/menu/menu-ip.sh" && chmod +x /usr/bin/menu-ip
+wget -q -O /usr/bin/menu-tor "https://raw.githubusercontent.com/annelyah23/snip/main/menu/menu-tor.sh" && chmod +x /usr/bin/menu-tor
+wget -q -O /usr/bin/autoboot "https://raw.githubusercontent.com/annelyah23/snip/main/menu/autoboot.sh" && chmod +x /usr/bin/autoboot
+wget -q -O /usr/bin/menu-tcp "https://raw.githubusercontent.com/annelyah23/snip/main/menu/menu-tcp.sh" && chmod +x /usr/bin/menu-tcp
+wget -q -O /usr/bin/rebootvps "https://raw.githubusercontent.com/annelyah23/snip/main/menu/rebootvps.sh" && chmod +x /usr/bin/rebootvps
+wget -q -O /usr/bin/menu-dns "https://raw.githubusercontent.com/annelyah23/snip/main/menu/menu-dns.sh" && chmod +x /usr/bin/menu-dns
 wget -q -O /usr/bin/netflix "https://raw.githubusercontent.com/syfqsamvpn/netflix/main/netflix.sh" && chmod +x /usr/bin/netflix
-wget -q -O /usr/bin/info "https://raw.githubusercontent.com/bakulssh/arismaramar/main/menu/info.sh" && chmod +x /usr/bin/info
-wget -q -O /usr/bin/mspeed "https://raw.githubusercontent.com/bakulssh/arismaramar/main/menu/menu-speedtest.sh" && chmod +x /usr/bin/mspeed
-wget -q -O /usr/bin/mbandwith "https://raw.githubusercontent.com/arismaramar/multi/main/menu/menu-bandwith.sh" && chmod +x /usr/bin/mbandwith
-wget -q -O /usr/bin/restart "https://raw.githubusercontent.com/arismaramar/multi/main/menu/restart.sh" && chmod +x /usr/bin/restart
-wget -q -O /usr/bin/update "https://raw.githubusercontent.com/arismaramar/multi/main/menu/update.sh" && chmod +x /usr/bin/update
+wget -q -O /usr/bin/info "https://raw.githubusercontent.com/annelyah23/snip/main/menu/info.sh" && chmod +x /usr/bin/info
+wget -q -O /usr/bin/mspeed "https://raw.githubusercontent.com/annelyah23/snip/main/menu/menu-speedtest.sh" && chmod +x /usr/bin/mspeed
+wget -q -O /usr/bin/mbandwith "https://raw.githubusercontent.com/annelyah23/snip/main/menu/menu-bandwith.sh" && chmod +x /usr/bin/mbandwith
+wget -q -O /usr/bin/restart "https://raw.githubusercontent.com/annelyah23/snip/main/menu/restart.sh" && chmod +x /usr/bin/restart
+wget -q -O /usr/bin/update "https://raw.githubusercontent.com/annelyah23/snip/main/menu/update.sh" && chmod +x /usr/bin/update
 echo -e " [INFO] Update Successfully"
 sleep 2
 exit
