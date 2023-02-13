3a325d2451c711ed936603c702ee33ef6a2762b7
annelyah <119406815+annelyah23@users.noreply.github.com>
2023-02-13 10:08:15 +0800

    Update menu-ip.sh

--- menu/menu-ip.sh
+++ menu/menu-ip.sh
@@ -473,7 +473,7 @@ echo -e ""
 read -n 1 -s -r -p "   Press any key to back on menu"
 menu-ip  
 }
-Isadmin=$(curl -sS https://raw.githubusercontent.com/arismaramar/permission/main/ipmini | grep $MYIP | awk '{print $5}')
+Isadmin=$(curl -sS https://raw.githubusercontent.com/annelyah23/permission/main/ipmini | grep $MYIP | awk '{print $5}')
 if [ "$Isadmin" = "OFF" ]; then
 clear
 echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
