0199ec540c0a2825cf6c2c6d588f12ae6479ffda
annelyah <119406815+annelyah23@users.noreply.github.com>
2023-02-17 20:05:16 +0800

    name

--- menu/menu-xtls.sh
+++ menu/menu-xtls.sh
@@ -86,10 +86,10 @@ echo -e "$COLOR1└────────────────────�
 echo -e ""
 echo -ne " ${WH}Select menu ${COLOR1}: ${WH}"; read opt
 case $opt in
-01 | 1) clear ; add ;;
-02 | 2) clear ; rnw ;;
-03 | 3) clear ; del ;;
-04 | 4) clear ; chk ;;
+01 | 1) clear ; addxtls ;;
+02 | 2) clear ; rnwxtls ;;
+03 | 3) clear ; delxtls ;;
+04 | 4) clear ; useronline ;;
 00 | 0) clear ; menu ;;
 *) clear ; menu-xtls ;;
 esac
