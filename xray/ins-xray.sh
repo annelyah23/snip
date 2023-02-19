68dc7f2deb78f2fc92205cb0df5397628330af07
annelyah <119406815+annelyah23@users.noreply.github.com>
2023-02-19 22:03:08 +0800

    Update ins-xray.sh

--- xray/ins-xray.sh
+++ xray/ins-xray.sh
@@ -129,12 +129,12 @@ bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release
 ## crt xray
 systemctl stop nginx
 mkdir /root/.acme.sh
-curl https://acme-install.netlify.app/acme.sh -O /root/acme.sh
-chmod +x /root/acme.sh
-/root/acme.sh --upgrade --auto-upgrade
-/root/acme.sh --set-default-ca --server letsencrypt
-/root/acme.sh --issue -d $domain --standalone -k ec-256
-~/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
+curl https://acme-install.netlify.app/acme.sh -o /root/.acme.sh/acme.sh
+chmod +x /root/.acme.sh/acme.sh
+/root/.acme.sh/acme.sh --upgrade --auto-upgrade
+/root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
 
 # nginx renew ssl
 echo -n '#!/bin/bash
